c5cb83c9942a55caa273e027896bf9647609632e
saysa <sabyasachi7629@gmail.com>
2026-07-19 21:15:46 +0530

    add banner

--- redis-setup/index.js
+++ redis-setup/index.js
@@ -4,8 +4,11 @@ import mongoose, { mongo } from 'mongoose'
 
 const app = express()
 
-const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
 
+
+
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+const BANNER_KEY = "app:banner";
 app.get('/redis', async(req, res) => {
     const reply = await redis.ping()
     res.json({
@@ -28,6 +31,30 @@ app.get('/mongo', async(req,res) => {
             error: error.message
         })
     }
+})
+
+app.post("/banner", async(req,res)=> {
+    try {
+        await redis.set(BANNER_KEY, req.body.message || "welcome to website");
+        res.json({success : true});
+    } catch (error) {
+        res.json({error : error.message})
+    }
+})
+app.use(express.json());
+app.get("/banner", async (req,res) => {
+    const message = await redis.get(BANNER_KEY);
+    res.json({message});
+})
+
+app.delete("/banner", async(req,res) => {
+    await redis.del(BANNER_KEY);
+    res.json({message});
+})
+
+app.get("/banner/exists", async(req, res) => {
+    const exists = await redis.exists(BANNER_KEY);
+    res.json({ exists: Boolean(exists)})
 })
 
 app.listen(3000, () => {
